ac194b29b4b95ae718aa75b08d3544a2ab178e7f
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-01 14:10:59 +0300

    Задание 2. Таблицы на уровнь вывела. Задание 3. Создано веб-приложение на основе получившейся верстки, которое на любой GET-запрос будет возвращать страницу из Задания 1.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+# Для начала определим настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    @staticmethod
+    def get_index():
+        return '''
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -353,4 +370,30 @@
     })()
 </script>
 </body>
-</html>
+</html>'''
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        page_content = self.get_index()
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
